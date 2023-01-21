bb0db91fc4a5d92528f1fb2a225a2add93e3f2bb
adrianlam15 <93681064+adrianlam15@users.noreply.github.com>
2023-01-21 14:28:24 -0800

    main python file for API

---
+++ main.py
@@ -0,0 +1,51 @@
+# improts
+from flask import Flask, request
+import os
+import openai
+
+openai.api_key = "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"
+
+# flask app
+app = Flask(__name__)
+
+# base API
+@app.route("/", methods=["GET"])
+def api():
+    if request.method == "GET":
+        return {"method": "GET"}
+
+
+# POST messages
+@app.route("/postmsg", methods=["POST"])
+def postMsg():
+    if request.method == "POST":
+        message = request.json[
+            "msg"
+        ]  # make sure to send a json with a key of "msg" and JS function handles submit
+        message = message + "\n\nTl;dr"
+        token_num = len(message) // 4
+        max_token = token_num // 2
+
+        genResponse = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=message,
+            temperature=0.7,
+            max_tokens=max_token,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=1,
+        )
+        return {"method": "POST", "message": message, "response": genResponse}
+
+
+# GET messages
+@app.route("/getmsg", methods=["GET"])
+def getMsg():
+    if request.method == "GET":
+        return {"method": "GET"}
+
+
+# main
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
