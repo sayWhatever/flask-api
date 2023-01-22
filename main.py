131decf6769d8b0e46eda4ae3eb89c145c340632
adrianlam15 <93681064+adrianlam15@users.noreply.github.com>
2023-01-21 18:49:33 -0800

    added specific channels for receiving and sending completion messages

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from flask_socketio import SocketIO, send, emit
 from flask_cors import CORS
 import openai
 
-
+# openai api key
 openai.api_key = "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"
 
 # flask app
@@ -13,52 +13,32 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 socketio = SocketIO(app, engineio_logger=True, logger=True, cors_allowed_origins="*")
 
 
-@socketio.on("ping")
-def handle_json():
-    print("after connect")
-    emit("response", {"data": "connect"})
+# channel for receiving messages
+@socketio.on("recMsg")
+def storeMsg(sentMsg):
+    with open("messages.txt", "a") as file_object:
+        file_object.write(sentMsg + "\n")
+    emit("messageSuccess", {"success": "true"})
 
 
-@socketio.on("summarize")
-def summarize(json):
-    emit("response", {"data": "summarize"})
-
-
-"""# base API
-@app.route("/", methods=["GET"])
-def api():
-    if request.method == "GET":
-        return {"method": "GET"}
-
-
-# POST messages
-@app.route("/postmsg", methods=["POST"])
-def postMsg():
-    if request.method == "POST":
-        message = request.json[
-            "msg"
-        ]  # make sure to send a json with a key of "msg" and JS function handles submit
-        message = message + "\n\nTl;dr"
-        token_num = len(message) // 4
-        max_token = token_num // 2
-
-        genResponse = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=message,
-            temperature=0.7,
-            max_tokens=max_token,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=1,
-        )
-        return {"method": "POST", "message": message, "response": genResponse}
-
-
-# GET messages
-@app.route("/getmsg", methods=["GET"])
-def getMsg():
-    if request.method == "GET":
-        return {"method": "GET"}"""
+# channel for sending summarized messages
+@socketio.on("sumMsg")
+def sumMsg():
+    with open("messages.txt", "r") as file_object:
+        msg = file_object.read()
+    msg = msg + "\n\nTl;dr"
+    token_num = len(msg) // 4
+    max_token = token_num // 2
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=msg,
+        temperature=0.7,
+        max_tokens=max_token,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=1,
+    )
+    emit("sumMsg", {"response": response})
 
 
 # main
